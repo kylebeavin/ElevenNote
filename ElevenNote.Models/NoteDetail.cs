298d3b2c35fd9e284bddb313243d0e9807f9c266
kyle beavin <kylebeavin@gmail.com>
2018-06-07 15:42:27 -0400

    kb

---
+++ ElevenNote.Models/NoteDetail.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevenNote.Models
+{
+    class NoteDetail
+    {
+        [Display(Name="Created")]
+        public int NoteId { get; set; }
+        [Display(Name="Modified")]
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTimeOffset CreatedUtc { get; set; }
+        public DateTimeOffset ModifiedUtc { get; set; }
+        public override string ToString() => $"[{NoteId}] {Title}";
+    }
+}
